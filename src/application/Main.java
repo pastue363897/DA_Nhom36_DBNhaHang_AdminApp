ffea49a0f78f31283e925b5df29473f1184d2e38
Dinh Van Dung <dinhvandungyknb6789@gmail.com>
2020-04-26 08:02:12 +0700

    [fix] update screen home admin

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,15 @@
 package application;
 	
+import java.io.IOException;
 import java.util.List;
 
 import database.AccountDAO;
 import entites.Account;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -14,18 +18,19 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+      Parent root = FXMLLoader.load(getClass().getResource("/view/HomeManager.fxml"));
+      primaryStage.setTitle("Hệ thống quản lý đặt bàn nhà hàng");
+      Scene scene = new Scene(root);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+		
 	}
 	
 	public static void main(String[] args) {
-		testConnect();
+		//testConnect();
 		launch(args);
 	}
 	
