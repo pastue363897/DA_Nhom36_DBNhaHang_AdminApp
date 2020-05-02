e201c48c15e58a33e8e874577f38975cb8588177
Dinh Van Dung <dinhvandungyknb6789@gmail.com>
2020-05-03 01:01:17 +0700

    [fix] update change TextField to PasswordField in LoginController

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -15,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -22,7 +23,7 @@ public class LoginController {
 	@FXML
 	private TextField txtUserName;
 	@FXML
-	private TextField txtPassWord;
+	private PasswordField txtPassWord;
 	@FXML
 	private Button btnSignin;
 
