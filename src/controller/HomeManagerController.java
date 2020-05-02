446af1124cb6848ef4571f627143bde0bc613343
Ta Khanh Hoang <pastue363897@gmail.com>
2020-05-02 21:40:25 +0700

    [add] thêm chức năng sign in và sign out cho admin

--- src/controller/HomeManagerController.java
+++ src/controller/HomeManagerController.java
@@ -1,57 +1,79 @@
 /**
  * Created on: 22:08:18 25 thg 4, 2020
  * 
- * @author Dinh Van Dung YKNB
+ * @author Dinh Van Dung YKNB, Ta Khanh Hoang
  */
 
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class HomeManagerController implements Initializable{
-  @FXML
-  private Button btnBanDatManager;
-  @FXML
-  private Button btnBanAnManager;
-  @FXML
-  private Button btnMonAnManager;
+public class HomeManagerController implements Initializable {
+	@FXML
+	private Button btnBanDatManager;
+	@FXML
+	private Button btnBanAnManager;
+	@FXML
+	private Button btnMonAnManager;
+	@FXML
+	private Button btnSignOut;
 
-  @FXML
-  private AnchorPane banDat;
-  @FXML
-  private AnchorPane banAn;
-  @FXML
-  private AnchorPane monAn;
-  
-  
-  public void navigationBar(ActionEvent e) {
-    if (e.getSource() == btnBanDatManager) {
-      banDat.setVisible(true);
-      banAn.setVisible(false);
-      monAn.setVisible(false);
-    } else if (e.getSource() == btnBanAnManager) {
-      banDat.setVisible(false);
-      banAn.setVisible(true);
-      monAn.setVisible(false);
-    } else if (e.getSource() == btnMonAnManager) {
-      banDat.setVisible(false);
-      banAn.setVisible(false);
-      monAn.setVisible(true);
-    }
-  }
+	@FXML
+	private AnchorPane banDat;
+	@FXML
+	private AnchorPane banAn;
+	@FXML
+	private AnchorPane monAn;
 
+	public void navigationBar(ActionEvent e) {
+		if (e.getSource() == btnBanDatManager) {
+			banDat.setVisible(true);
+			banAn.setVisible(false);
+			monAn.setVisible(false);
+		} else if (e.getSource() == btnBanAnManager) {
+			banDat.setVisible(false);
+			banAn.setVisible(true);
+			monAn.setVisible(false);
+		} else if (e.getSource() == btnMonAnManager) {
+			banDat.setVisible(false);
+			banAn.setVisible(false);
+			monAn.setVisible(true);
+		}
+	}
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    banDat.setVisible(true);
-    banAn.setVisible(false);
-    monAn.setVisible(false);
-  }
+	public void signOut(ActionEvent e) {
+		Stage current = (Stage) btnSignOut.getScene().getWindow();
+		current.close();
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("Hệ thống quản lý đặt bàn nhà hàng");
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		banDat.setVisible(true);
+		banAn.setVisible(false);
+		monAn.setVisible(false);
+	}
 }
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,20 +1,60 @@
 /**
  * Created on: 19:29:11 2 thg 5, 2020
- * @author Dinh Van Dung YKNB
+ * @author Dinh Van Dung YKNB, Ta Khanh Hoang
  */
 
 package controller;
 
+import java.io.IOException;
+
+import database.AdminDAO;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
-  @FXML
-  private TextField txtUserName;
-  @FXML
-  private TextField txtPassWord;
-  @FXML
-  private Button btnSignin;
-  
+	@FXML
+	private TextField txtUserName;
+	@FXML
+	private TextField txtPassWord;
+	@FXML
+	private Button btnSignin;
+
+	@FXML
+	void signIn(ActionEvent event) {
+		// dang nhap ko password de debug: nhap username: DEBUG_LOGIN_1F59AC46 rui sign in
+		String username = txtUserName.getText();
+		String password = txtPassWord.getText();
+		AdminDAO adminDao = new AdminDAO();
+		int ref = adminDao.signIn(username, password);
+		if (ref != 1) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Đăng nhập thất bại");
+			alert.setContentText("Sai username hay mật khẩu, hãy kiểm tra lại.");
+			alert.show();
+		} else {
+			try {
+				// Close sign in window
+				Stage currentStage = (Stage) btnSignin.getScene().getWindow();
+				currentStage.close();
+				// load main
+				Parent root = FXMLLoader.load(getClass().getResource("/view/HomeManager.fxml"));
+				Stage stage = new Stage();
+				stage.setTitle("Hệ thống quản lý đặt bàn nhà hàng");
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+	}
 }
